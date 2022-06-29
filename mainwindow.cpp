73a9cebbf30eac3e4a9b65fa314090c0ce486705
Xing Yi <2111027@mail.nankai.edu.cn>
2022-06-30 07:37:17 +0800

    added a menu bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
 	, ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+
+	createActions();
+	createMenus();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,53 @@ MainWindow::~MainWindow()
 	delete ui;
 }
 
+void MainWindow::createActions()
+{
+	newCollectionAction = new QAction("&New collection", this);
+
+	openCollectionAction = new QAction("&Open collection", this);
+
+	addItemAction = new QAction("&Add item", this);
+
+	exportAction = new QAction("&Export collection", this);
+
+	quitAction = new QAction("&Quit", this);
+	connect(quitAction, &QAction::triggered, QApplication::instance(), &QApplication::quit);
+
+	editMetadataAction = new QAction("&Edit metadata", this);
+
+	deleteItemAction = new QAction("&Delete item", this);
+
+	playAction = new QAction("&Play", this);
+
+	for (size_t i = 0; i < Film::NumAttributes; ++i)
+	{
+		sortActions[i] = new QAction(Film::NamesAttributes[i].c_str(), this);
+	}
+
+	aboutAction = new QAction("&About filibre");
+}
+
+void MainWindow::createMenus()
+{
+	fileMenu = menuBar()->addMenu("&File");
+	fileMenu->addAction(newCollectionAction);
+	fileMenu->addAction(openCollectionAction);
+	fileMenu->addAction(addItemAction);
+	fileMenu->addAction(exportAction);
+	fileMenu->addAction(quitAction);
+
+	editMenu = menuBar()->addMenu("&Edit");
+	editMenu->addAction(editMetadataAction);
+	editMenu->addAction(deleteItemAction);
+	editMenu->addAction(playAction);
+
+	sortMenu = menuBar()->addMenu("&Sort by");
+	for (size_t i = 0; i < Film::NumAttributes; ++i)
+	{
+		sortMenu->addAction(sortActions[i]);
+	}
+
+	aboutMenu = menuBar()->addMenu("&About");
+	aboutMenu->addAction(aboutAction);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include "filmcollection.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,32 @@ public:
 	MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 
+private slots:
+
+
 private:
 	Ui::MainWindow *ui;
+
+	QMenu * fileMenu;
+	QMenu * editMenu;
+	QMenu * sortMenu;
+	QMenu * aboutMenu;
+
+	QAction * newCollectionAction;
+	QAction * openCollectionAction;
+	QAction * addItemAction;
+	QAction * exportAction;
+	QAction * quitAction;
+
+	QAction * editMetadataAction;
+	QAction * deleteItemAction;
+	QAction * playAction;
+
+	QAction * sortActions[Film::NumAttributes];
+
+	QAction * aboutAction;
+
+	void createActions();
+	void createMenus();
 };
 #endif // MAINWINDOW_H
