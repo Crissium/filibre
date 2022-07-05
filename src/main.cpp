6d4b0de61b5bc6472ca2b898aeeb8f09d25582c6
Xing Yi <2111027@mail.nankai.edu.cn>
2022-07-05 12:06:56 +0800

    display a splash screen at startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "runguard.h"
+#include <ctime>
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +11,17 @@ int main(int argc, char *argv[])
 		return 0;
 
 	QApplication a(argc, argv);
+
+	time_t start = time(nullptr);
+
+	QPixmap splashPic(":/images/splash.png");
+	QSplashScreen splash(splashPic);
+	splash.show();
+
+	while (time(nullptr) - start < 1);
+
 	MainWindow w;
 	w.show();
+	splash.finish(&w);
 	return a.exec();
 }
